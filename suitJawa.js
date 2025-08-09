7cd6fc97353f9d56e3a649e01337f6dd68610884
Naufal Fauzan <l200230062@student.ums.ac.id>
2025-08-09 08:51:38 +0700

    skor feature

--- suitJawa.js
+++ suitJawa.js
@@ -44,10 +44,21 @@ function getHasilUser(user, comp) {
 const input = document.querySelectorAll(".input");
 
 for (let i = 0; i < input.length; i++) {
+  let skorComp = 0;
+  let skorUser = 0;
   input[i].addEventListener("click", function () {
     const value = input[i].getAttribute("value");
     const hasilComputer = getHasilComp();
     const hasilUser = getHasilUser(value, hasilComputer);
+    const tampilSkorComp = document.querySelector("#skorComp");
+    const tampilSkorUser = document.querySelector("#skorUser");
+
+    if (hasilUser == "IMBANG") {
+    } else if (hasilUser == "MENANG") {
+      skorUser += 1;
+    } else if (hasilUser == "KALAH") {
+      skorComp += 1;
+    }
 
     putar();
 
@@ -57,6 +68,9 @@ for (let i = 0; i < input.length; i++) {
 
       const tampilHasil = document.querySelector("#hasil");
       tampilHasil.innerHTML = hasilUser;
+
+      tampilSkorComp.innerHTML = "SKOR = " + skorComp;
+      tampilSkorUser.innerHTML = "SKOR = " + skorUser;
     }, 1000);
   });
 }
